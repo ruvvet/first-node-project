7fb56f3e3dd37d61cffe1626c97eb0c611994638
Jenny <finej01@gmail.com>
2020-11-12 15:07:58 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+//const { beBasic } = require("./myModule");
+//const myModule = require("./myModule");
+// myModule.add >>>> call the function on the module >> the key value pair of the object is key = function name, value = function
+
+const{add, sub} = require("./myModule"); // still has access even though specified only these 2?
+
 let name = 'jenny';
 console.log('jenny')
+
+console.log(beBasic());
+console.log(add(1,2), sub(2,1));
+
---
+++ myModule.js
@@ -0,0 +1,18 @@
+//module.exports.beBasic = () => "That's so fetch!"
+
+beBasic = () =>"That's so fetch!"
+
+function add(x,y){
+    return x +y;
+}
+
+function sub(x,y){
+    return x-y;
+}
+
+module.exports = {
+    beBasic,
+    add,
+    sub
+
+}
